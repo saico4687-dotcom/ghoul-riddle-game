1936768b2d572322e3ca832034b746e0f63d0423
saico4687-dotcom <saico4687@gmail.com>
2026-07-29 13:04:23 +0300

    Delete vendor/capacitor-community-admob directory

--- vendor/capacitor-community-admob/android/src/main/java/com/getcapacitor/community/admob/appopen/AppOpenAdPluginEvents.kt
+++
@@ -1,9 +0,0 @@
-package com.getcapacitor.community.admob.appopen
-
-object AppOpenAdPluginEvents {
-    const val Loaded = "appOpenAdLoaded"
-    const val FailedToLoad = "appOpenAdFailedToLoad"
-    const val Opened = "appOpenAdOpened"
-    const val Closed = "appOpenAdClosed"
-    const val FailedToShow = "appOpenAdFailedToShow"
-}
--- vendor/capacitor-community-admob/ios/Sources/AdMobPlugin/AppOpen/AppOpenAdPluginEvents.swift
+++
@@ -1,7 +0,0 @@
-public enum AppOpenAdPluginEvents: String {
-    case Loaded = "appOpenAdLoaded"
-    case FailedToLoad = "appOpenAdFailedToLoad"
-    case Opened = "appOpenAdOpened"
-    case Closed = "appOpenAdClosed"
-    case FailedToShow = "appOpenAdFailedToShow"
-}
