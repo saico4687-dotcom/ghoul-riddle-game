dc24991e5218bc11e1606c7a187cf6e41834078d
saico4687-dotcom <saico4687@gmail.com>
2026-08-01 03:17:24 +0300

    Revoke execute permissions on internal functions

---
+++ supabase/migrations/20260731234413_lock_down_internal_trigger_functions.sql
@@ -0,0 +1,6 @@
+revoke execute on function public.enforce_group_member_cap() from anon, authenticated;
+revoke execute on function public.enforce_group_message_edit_delete_window() from anon, authenticated;
+revoke execute on function public.enforce_message_edit_delete_window() from anon, authenticated;
+revoke execute on function public.set_group_message_expiry() from anon, authenticated;
+revoke execute on function public.set_message_expiry() from anon, authenticated;
+revoke execute on function public.purge_expired_stories() from anon, authenticated;
